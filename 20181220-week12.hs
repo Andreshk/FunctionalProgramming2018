0702bf1215c6cb67f013398395eef8547d538458
Andrey Drenski <andreshko.dre@gmail.com>
2019-01-03 22:37:09 +0200

    add week13 material

--- 20181220-week12.hs
+++ 20181220-week12.hs
@@ -41,18 +41,85 @@ rotateRight (Node q (Node p a b) c) = Node p a (Node q b c)
 -- treeMap _ Empty = Empty
 -- treeMap f (Node val l r) = Node (f val) (treeMap f l) (treeMap f r)
 
+-- Дърво с произволен брой наследници... не особено полезно
+data VTree a = VEmpty | VNode a [VTree a]
+instance Functor VTree where
+  fmap f VEmpty = VEmpty
+  fmap f (VNode val subtrees) = VNode (f val) (map (fmap f) subtrees)
+
 instance Functor Tree where
-    fmap _ Empty = Empty
-    fmap f (Node val l r) = Node (f val) (fmap f l) (fmap f r)
+  fmap _ Empty = Empty
+  fmap f (Node val l r) = Node (f val) (fmap f l) (fmap f r)
 
 -- Използваме друга структура, за да можем да я строим само със
 -- специални функции. Щом е друга структура, трябва имената на
 -- конструкторите да са други, също.
 data BST a = BEmpty | BNode a (BST a) (BST a)
+-- Ако искаме pretty-printing, трябва да инстанцираме Show
+-- и (в случая) функцията по подразбиране show да извиква нашата
+instance Show a => Show (BST a) where
+  show t = show' 0 t
+    where show' pad BEmpty = replicate pad ' ' ++ "#"
+          show' pad (BNode val l r) = show' (pad+2) l
+                                   ++ "\n"
+                                   ++ replicate pad ' ' ++ show val
+                                   ++ "\n"
+                                   ++ show' (pad+2) r
 
 -- Да кажем, че повтарянето на стойности е позволено
 bstInsert :: Ord a => a -> BST a -> BST a
 bstInsert x BEmpty = BNode x BEmpty BEmpty
-bstInsert x (Node val l r)
-  | x <= val  = Node val (bstInsert x l) r
-  | otherwise = Node val l (bstInsert x r)
+bstInsert x (BNode val l r)
+  | x <= val  = BNode val (bstInsert x l) r
+  | otherwise = BNode val l (bstInsert x r)
+
+bstFromList :: Ord a => [a] -> BST a
+bstFromList lst = foldr bstInsert BEmpty lst
+
+bstValues :: BST a -> [a]
+bstValues BEmpty = []
+bstValues (BNode val l r) = bstValues l ++ [val] ++ bstValues r
+
+bstSort :: Ord a => [a] -> [a]
+bstSort = bstValues . bstFromList
+
+data Map k v = MEmpty | MNode k v (Map k v) (Map k v)
+-- data Maybe a = Nothing | Just a
+mapSearch :: Ord k => k -> Map k v -> Maybe v
+mapSearch _ MEmpty = Nothing
+mapSearch k' (MNode k v l r)
+  | k' == k   = Just v
+  | k' < k    = mapSearch k' l
+  | otherwise = mapSearch k' r
+
+data Direction = L | R deriving Show
+
+bstPath :: Ord a => a -> BST a -> Maybe [Direction]
+bstPath _ BEmpty = Nothing
+bstPath x (BNode val l r)
+  | x == val  = Just []
+  | x < val   = (L:) <$> (bstPath x l) -- <$> = fmap, в случая инстанциран за Maybe
+  | otherwise = (R:) <$> (bstPath x r)
+{- Горното е съкращаване на следния pattern:
+  | x < val   = case bstPath x l of Nothing -> Nothing
+                                    Just path -> Just (L:path)
+  | otherwise = case bstPath x r of Nothing -> Nothing
+                                    Just path -> Just (R:path)
+-}
+
+-- Подобрение: отделните валути не са различни типове,
+-- но всяко число може да върви с валута като "етикет"
+data Currency = USD | BGN | EUR deriving Eq
+exchangeRate :: Currency -> Currency -> Float
+exchangeRate USD BGN = 1.6
+exchangeRate BGN USD = 1 / exchangeRate USD BGN
+exchangeRate EUR BGN = 1.95
+exchangeRate BGN EUR = 1 / exchangeRate BGN EUR
+exchangeRate USD EUR = exchangeRate USD BGN * exchangeRate BGN EUR
+
+type Sum = (Float,Currency)
+exchange :: Sum -> Currency -> Sum
+exchange (x,c) c'
+  | c == c'   = (x,c)
+  | otherwise = (x*r, c')
+  where r = exchangeRate c c'
